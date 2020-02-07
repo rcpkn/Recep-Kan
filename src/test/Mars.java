992e7b5a92656596e93125c9e6eece187e67ad98
SyntaxTechnologies <instructor@syntaxtechs.com>
2020-02-06 19:46:27 -0500

    project created

---
+++ src/test/Mars.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Mars {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+
+		
+		
+		
+		int a;
+	System.out.println("Hello");	
+		
+		
+		
+	}
+
+}
